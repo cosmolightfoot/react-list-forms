70b31a83b06e45203379b30ea18fbe4ff0d658da
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-16 14:41:49 -0700

    CreateColor component written

--- src/components/CreateColor.js
+++ src/components/CreateColor.js
@@ -1,10 +1,37 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
-export default class CreateColor {
+export default class CreateColor extends PureComponent {
+  static propTypes = {
+    addColor: PropTypes.func.isRequired
+  }
+  
   state = {
     name: '',
-    hex: ''
+    color: '#000000'
   }
 
-  
+  handleSubmit = ({ preventDefault }) => {
+    preventDefault();
+    const { name, color } = this.state;
+    
+    console.log({ name, hex: color })
+    this.props.addColor({ name, hex: color });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { name, color } = this.state;
+    
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input name="name" value={name} onChange={this.handleChange} />
+        <input type="color" name="color" value={color} onChange={this.handleChange} />
+        <button>Add Color</button>
+      </form>
+    );
+  }  
 }
