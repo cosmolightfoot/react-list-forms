a1c75d8c3460aae512509c047db2665328fe6aa8
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-16 14:51:37 -0700

    Color component is written

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CreateColor from './CreateColor.js';
+import Color from './Color.js';
 
 
 export default function App() {
   return (
     <>
       <CreateColor />
+      <Color name="black" hex="#000000" />
     </>
   );
 }
--- src/components/Color.js
+++ src/components/Color.js
@@ -1,3 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+const decimalFromHex = hex => {
+  return Number.parseInt(`0x${hex}`, 16);
+};
 
+const rgbFromHex = hex => {
+  return {
+    red: decimalFromHex(hex.slice(1, 3)),
+    green: decimalFromHex(hex.slice(3, 5)),
+    blue: decimalFromHex(hex.slice(5, 7)),
+  };
+};
+
+export default function Color({ name, hex }) {
+  const rgb = rgbFromHex(hex);
+
+  const colorStyle = {
+    display: 'inline-block',
+    backgroundColor: hex,
+    width: '0.8em',
+    height: '0.8em'
+  };
+
+  return (
+    <dl>
+      <dt>Color:</dt>
+      <dd>{name}<div style={colorStyle}></div></dd>
+
+      <dt>Hex:</dt>
+      <dd>{hex}</dd>
+
+      <dt>Red:</dt>
+      <dd>{rgb.red}</dd>
+
+      <dt>Green:</dt>
+      <dd>{rgb.green}</dd>
+
+      <dt>Blue:</dt>
+      <dd>{rgb.blue}</dd>
+    </dl>
+  );
+}
+
+Color.propTypes = {
+  name: PropTypes.string.isRequired,
+  hex: PropTypes.string.isRequired
+};
