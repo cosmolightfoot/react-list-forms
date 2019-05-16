18ecf3eddd51289a9b6b5d1acbb2f58f001c82c3
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-16 13:25:25 -0700

    exercise completed

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextFormatter from './TextFormatter.js';
+
+export default function App() {
+  return (
+    <>
+      <TextFormatter />
+    </>
+  );
+}
--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -1,19 +1,41 @@
 import React, { PureComponent } from 'react';
-// import figlet from 'figlet';
+import figlet from 'figlet';
 
-export default class textFormatter extends PureComponent {
+export default class TextFormatter extends PureComponent {
   state = {
-    text: ''
+    text: '',
+    color: 'black',
+    font: '',
+    formattedText: ''
   }
 
   handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value });
-  } 
+    this.setState({ [target.name]: target.value }, () => {
+      this.formatText();
+    });
+  }
+
+  formatText = () => {
+    const { text, font } = this.state;
+    figlet.text(text, { font: font }, (err, result) => {
+      this.setState({ formattedText: result });
+    });
+  }
 
   render() {
+    const { text, color, font, formattedText } = this.state;
+
+    const fontOptions = ['Alligator', 'Alpha', 'Barbwire', 'Banner4', 'Isometric3'].map(font => {
+      return (<option key={font} value={font}>{font}</option>);
+    });
 
     return (
+      <>
+      <select name="font" value={font} onChange={this.handleChange}>{fontOptions}</select>
       <input name="text" value={text} onChange={this.handleChange} />
+      <input name="color" type="color" value={color} onChange={this.handleChange} />
+      <pre style={{ color }}>{formattedText}</pre>
+      </>
     );
   }
 }
